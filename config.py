4f413c12465d58170257c7a9b77c76122e629bd2
Branganaupul <81672222+upulranganab@users.noreply.github.com>
2023-03-15 14:12:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ API_SECRET = 'yourapisecret'
 # binance future testnet
 API_TEST_KEY ='f236c8ffb10ae1b205741a3ba850dc6e7d65572814ee1505ff4969562bae66b3'
 API_TEST_SECRET ='f54e4b4f7db5ceb4857c1905fb4274156ab13fc943638ed001c46c71dceb25b2'
+
+long_entry_per = 0.90
+# Long TP
+long_tp_per = 0.94
+# Short entry
+short_entry_per = -0.90
+# Short TP
+short_tp_per= -0.94
