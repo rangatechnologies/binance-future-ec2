6138371f180d8c7db77df06e83aa28031f71fca1
Grecu92 <127939172+Grecu92@users.noreply.github.com>
2023-03-15 12:19:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 API_KEY = 'yourapikey'
 API_SECRET = 'yourapisecret'
 # binance future testnet
-API_TEST_KEY ='f236c8ffb10ae1b205741a3ba850dc6e7d65572814ee1505ff4969562bae66b3'
-API_TEST_SECRET ='f54e4b4f7db5ceb4857c1905fb4274156ab13fc943638ed001c46c71dceb25b2'
+API_TEST_KEY ='lubzmk3mAlwu465XFr6VFVcPAHsm1695s5ZCTMZZNEV4AX2DGC5AOsKDU2LuCzqC'
+API_TEST_SECRET ='iOj7f40SaVoSanKNZ70u7vJMZuxfwHfpfAMbl5frosdM2kALBqUFm6CViIlQcmg9'
 
 long_entry_per = 0.90
 # Long TP
