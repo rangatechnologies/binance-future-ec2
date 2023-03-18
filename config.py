0e0f2176cf19034b8c346152b01032c9d801deaa
Grecu92 <127939172+Grecu92@users.noreply.github.com>
2023-03-18 09:44:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ API_SECRET = 'yourapisecret'
 API_TEST_KEY ='lubzmk3mAlwu465XFr6VFVcPAHsm1695s5ZCTMZZNEV4AX2DGC5AOsKDU2LuCzqC'
 API_TEST_SECRET ='iOj7f40SaVoSanKNZ70u7vJMZuxfwHfpfAMbl5frosdM2kALBqUFm6CViIlQcmg9'
 
-long_entry_per = 0.90
+long_entry_per = 0.10
 # Long TP
-long_tp_per = 0.94
+long_tp_per = 0.30
 # Short entry
-short_entry_per = -0.90
+short_entry_per = -0.10
 # Short TP
-short_tp_per= -0.94
+short_tp_per= -0.30
